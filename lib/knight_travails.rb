60330322c21248f85baaa7d3a809ea15161da9c5
Stefano Rondelli <rstefanofil@hotmail.com>
2022-04-10 17:44:10 +0200

    Add move functionality
    Does not currently check legality

--- lib/knight_travails.rb
+++ lib/knight_travails.rb
@@ -2,24 +2,11 @@ module Moves
 
 end
 
-class Board
-    attr_accessor :board
+module Navigation
 
     LETTERS = ('A'..'H').to_a
     NUMBERS = (1..8).to_a
     BOARD_UI = LETTERS.map { | l | NUMBERS.map { | n | "#{l}#{n}" } }.flatten
-
-    def initialize
-        @board = make_board
-    end
-
-    def make_board
-        Array.new(64) { | i | i += 1 }
-    end
-
-    def display_board
-        
-    end
 
     def convert_back_to_front(index)
         converted = BOARD_UI[index]
@@ -33,16 +20,42 @@ class Board
 
 end
 
+class Board
+    attr_accessor :board
+    include Navigation
+
+    def initialize
+        @board = make_board
+    end
+
+    def make_board
+        Array.new(64, nil)
+    end
+
+    def display_board
+
+    end
+
+end
+
 
 
 class Knight
     attr_accessor :position
-    include Moves
-    ALLOWED_MOVES = [10, -15, -6, 17, 15, 6, -10, 17]
+    include Moves, Navigation
+    STANDARD_MOVESET = [10, -15, -6, 17, 15, 6, -10, 17]
 
-    def initialize(player = 1)
-        @position = @board.sample
+    def initialize(board, player = 1)
         @player = player
+        @symbol = "K"
+        @position = rand(0..63)
+        board.board[@position] = @symbol
+    end
+
+    def move_piece(board, destination)
+        board.board[@position] = nil
+        @position = destination
+        board.board[destination] = @symbol
     end
 
 end
@@ -50,5 +63,9 @@ end
 
 chess = Board.new
 chess.make_board
-chess.convert_back_to_front(3)
-chess.convert_front_to_back("C7")
+horsey = Knight.new(chess)
+puts horsey.position
+puts horsey.convert_back_to_front(horsey.position)
+horsey.move_piece(chess, 53)
+puts horsey.position
+puts horsey.convert_back_to_front(horsey.position)
